9271acdc5e2a46998854fb989e47f30fd8e1f3f9
krpranav7 <pranavkr9416@gmail.com>
2026-09-02 10:02:06 +0530

    Change themeToggle emoji to icon

--- frontend/src/components/ui/ThemeToggle.tsx
+++ frontend/src/components/ui/ThemeToggle.tsx
@@ -1,11 +1,12 @@
-import { useTheme } from "../../context/ThemeContext";
+import { useTheme } from "../../context/ThemeContext"
+import { Moon, Sun } from 'lucide-react'
 
 export function ThemeToggle() {
     const { theme, toggleTheme } = useTheme() // works bcz whole app is wrapped with ThemeProvider context API
 
     return (
         <button onClick={toggleTheme} aria-label="Toggle theme" className="p-1.5 rounded-full bg-slate-100 dark:bg-slate-800 text-slate-700 dark:text-slate-200 hover:bg-slate-200 dark:hover:bg-slate-700 transition-colors">
-            {theme === 'light' ? <span className="inline-block w-6 p-0 text-sm">⏾</span> : <span className="inline-block w-6">🔆</span>}
+            {theme === 'light' ? <Moon size={18} /> : <Sun size={18} />}
         </button>
     )
 }
