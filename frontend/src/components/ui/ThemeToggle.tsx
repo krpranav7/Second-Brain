1169c74eb30b345f7b9db5a173c83597bcd16a23
krpranav7 <pranavkr9416@gmail.com>
2026-09-07 17:11:18 +0530

    scale theme toggle on hover

--- frontend/src/components/ui/ThemeToggle.tsx
+++ frontend/src/components/ui/ThemeToggle.tsx
@@ -5,8 +5,8 @@ export function ThemeToggle() {
     const { theme, toggleTheme } = useTheme() // works bcz whole app is wrapped with ThemeProvider context API
 
     return (
-        <button onClick={toggleTheme} aria-label="Toggle theme" className="cursor-pointer p-1.5 rounded-full bg-slate-100 dark:bg-neutral-800 text-slate-700 dark:text-neutral-200 hover:bg-slate-200 dark:hover:bg-neutral-700 transition-colors">
-            {theme === 'light' ? <Moon size={18} className="fill-blue-900" /> : <Sun className="text-yellow-400" size={18} />}
+        <button onClick={toggleTheme} aria-label="Toggle theme" className="cursor-pointer p-1.5 rounded-full bg-slate-100 dark:bg-neutral-800 text-slate-700 dark:text-neutral-200 hover:bg-slate-200 dark:hover:bg-neutral-700 transition-colors hover:scale-105">
+            {theme === 'light' ? <Moon size={17.5} className="fill-blue-900" /> : <Sun className="text-yellow-400" size={17.5} />}
         </button>
     )
 }
